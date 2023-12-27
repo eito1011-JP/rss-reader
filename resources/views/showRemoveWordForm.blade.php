45a038f149e4e8ab7576d42135edda2c8be0977b
eito1011-JP <eito.55855@gmail.com>
2023-12-28 02:24:27 +0900

    fix

--- resources/views/showRemoveWordForm.blade.php
+++ resources/views/showRemoveWordForm.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="ja">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -7,46 +7,61 @@
         <title>RSS READER</title>
     </head>
     <body class="antialiased">
-      <form method="POST" action="{{ route('remove.words') }}">
+    <form method="POST" action="{{ route('remove.words') }}">
             @csrf
-            <div id="input-fields">
-                <div class="mb-3">
-                  <label for="url" class="form-label">RSSのURLを入力してください</label>
-                  <input class="url" id="url" type="text" name="url[]" class="form-control">
-                </div>
-                <div class="mb-3">
-                    <label for="remove-words" class="form-label">削除したい文字を入力してください(半角空白で区切ると複数の単語を一括で指定できます)</label>
-                    <input class="remove-words" id="remove-words" type="text" name="remove_words[]" class="form-control">
+            <div id="input_fields">
+                <div id="template">
+                    <div class="mb-3">
+                        <label for="url" class="form-label">RSS</label>
+                        <input class="url" id="url" type="text" name="url[]" class="form-control" placeholder="https://sample.com/rss">
+                    </div>
+                    <div class="mb-3">
+                        <label for="remove-words" class="form-label">削除する単語</label>
+                        <input class="remove-words" id="remove_words" type="text" name="remove_words[]" class="form-control" placeholder="backend frontend...">
+                    </div>
+                    <input type="checkbox" class="delete-checkbox" id="check_box" onclick="removeField(this)">
                 </div>
             </div>
             <button type="button" id="addBtn" class="btn btn-primary">追加</button>
             <button type="submit" class="btn btn-primary">Submit</button>
-      </form>
+    </form>
     </body>
     <script>
-  document.getElementById('addBtn').addEventListener('click', function() {
-                var inputFields = document.getElementById('input-fields');
-                var fieldIndex = document.querySelectorAll('input[name="url[]"]').length;
-                
-                var urlDiv = document.createElement('div');
-                urlDiv.className = 'mb-3';
-                urlDiv.innerHTML = '<label class="form-label" for="url' + fieldIndex + '">URL</label>' +
-            '<input type="text" name="url[]" class="form-control" id="url' + fieldIndex + '">' +
-            '<div class="error-txt" id="error-url' + fieldIndex + '"></div>';
+    document.getElementById('addBtn').addEventListener('click', function() {
+        var inputFields = document.getElementById('input_fields');
+        var fieldIndex = document.querySelectorAll('input[name="url[]"]').length;
+        var fragment = document.createDocumentFragment();
+        var template = document.createElement('div');
+        template.id = 'template' + fieldIndex;
+        template.innerHTML = '<div class="mb-3">' +
+        '<label class="form-label" for="url' + fieldIndex + '">RSS </label>' +
+        '<input type="text" name="url[]" class="form-control" id="url' + fieldIndex + '" placeholder="https://sample.com/rss">' + 
+        '</div>' +
+        '<div class="mb-3" style="margin: 1rem;">' +
+        '<label class="form-label" for="remove_words' + fieldIndex + '">削除する単語 </label>' +
+        '<input type="text" name="remove_words[]" class="form-control" id="remove_words' + fieldIndex + '" placeholder="backend frontend...">' +
+        '</div>' +
+        '<input style="translate: -1rem -3.7rem;" type="checkbox" class="delete-checkbox" id="check_box' + fieldIndex + '" onclick="removeField(this)">';
 
-                var wordsDiv = document.createElement('div');
-                wordsDiv.className = 'mb-3';
-                wordsDiv.innerHTML = '<label class="form-label" for="remove_words' + fieldIndex + '">The Words you remove</label>' +
-            '<input type="text" name="remove_words[]" class="form-control" id="remove_words' + fieldIndex + '">' +
-            '<div class="error-txt" id="error-remove-words' + fieldIndex + '"></div>';
+    fragment.appendChild(template);
+    inputFields.appendChild(fragment);
+    });
 
-                inputFields.appendChild(urlDiv);
-                inputFields.appendChild(wordsDiv);
-            });
+    function removeField(checkBox) {
+        var fieldIndex = checkBox.id.replace(/[^0-9]/g, '');
+        var template = document.getElementById('template' + fieldIndex);
+        template.remove();
+    }
 </script>
 <style>
-  .mb-3 {
+    .mb-3 {
         margin: 1rem;
+    }
+    div#input_fields {
+        margin-left: 1rem;
+    }
+    input#check_box {
+        translate: -1rem -3.7rem;
     }
 </style>
 </html>
